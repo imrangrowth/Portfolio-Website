33cde80df5c745edc1ed651b9cfac796ed0f1b9b
Imranmuhaa <imranmuha43@gmail.com>
2025-07-16 17:35:27 +0100

    Update initialFX.ts

--- src/components/utils/initialFX.ts
+++ src/components/utils/initialFX.ts
@@ -1,53 +1,39 @@
-import { SplitText } from "gsap-trial/SplitText";
-import gsap from "gsap";
+import { gsap } from "gsap";
+import { smoother } from "../Navbar";
 
 export function initialFX() {
   document.body.style.overflowY = "auto";
-
-  // Remove smoother.paused(false);
-
+  smoother.paused(false);
   document.getElementsByTagName("main")[0].classList.add("main-active");
+
   gsap.to("body", {
     backgroundColor: "#0b080c",
     duration: 0.5,
     delay: 1,
   });
 
-  let landingText = new SplitText(
+  gsap.fromTo(
     [".landing-info h3", ".landing-intro h2", ".landing-intro h1"],
-    {
-      type: "chars,lines",
-      linesClass: "split-line",
-    }
-  );
-  gsap.fromTo(
-    landingText.chars,
-    { opacity: 0, y: 80, filter: "blur(5px)" },
+    { opacity: 0, y: 60 },
     {
       opacity: 1,
-      duration: 1.2,
-      filter: "blur(0px)",
-      ease: "power3.inOut",
       y: 0,
-      stagger: 0.025,
+      duration: 1.2,
+      ease: "power3.out",
+      stagger: 0.15,
       delay: 0.3,
     }
   );
 
-  const TextProps = { type: "chars,lines", linesClass: "split-h2" };
-
-  let landingText2 = new SplitText(".landing-h2-info", TextProps);
   gsap.fromTo(
-    landingText2.chars,
-    { opacity: 0, y: 80, filter: "blur(5px)" },
+    ".landing-h2-info",
+    { opacity: 0, y: 60 },
     {
       opacity: 1,
-      duration: 1.2,
-      filter: "blur(0px)",
-      ease: "power3.inOut",
       y: 0,
-      stagger: 0.025,
-      delay: 0.3,
+      duration: 1.2,
+      ease: "power3.out",
+      delay: 1.5,
     }
   );
 
@@ -56,82 +42,21 @@ export function initialFX() {
     { opacity: 0, y: 30 },
     {
       opacity: 1,
-      duration: 1.2,
-      ease: "power1.inOut",
       y: 0,
-      delay: 0.8,
+      duration: 1,
+      ease: "power2.out",
+      delay: 2,
     }
   );
+
   gsap.fromTo(
     [".header", ".icons-section", ".nav-fade"],
     { opacity: 0 },
     {
       opacity: 1,
       duration: 1.2,
-      ease: "power1.inOut",
-      delay: 0.1,
+      ease: "power1.out",
+      delay: 0.8,
     }
   );
-
-  let landingText3 = new SplitText(".landing-h2-info-1", TextProps);
-  let landingText4 = new SplitText(".landing-h2-1", TextProps);
-  let landingText5 = new SplitText(".landing-h2-2", TextProps);
-
-  LoopText(landingText2, landingText3);
-  LoopText(landingText4, landingText5);
-}
-
-function LoopText(Text1: SplitText, Text2: SplitText) {
-  let tl = gsap.timeline({ repeat: -1, repeatDelay: 1 });
-  const delay = 4;
-  const delay2 = delay * 2 + 1;
-
-  tl.fromTo(
-    Text2.chars,
-    { opacity: 0, y: 80 },
-    {
-      opacity: 1,
-      duration: 1.2,
-      ease: "power3.inOut",
-      y: 0,
-      stagger: 0.1,
-      delay: delay,
-    },
-    0
-  )
-    .fromTo(
-      Text1.chars,
-      { y: 80 },
-      {
-        duration: 1.2,
-        ease: "power3.inOut",
-        y: 0,
-        stagger: 0.1,
-        delay: delay2,
-      },
-      1
-    )
-    .fromTo(
-      Text1.chars,
-      { y: 0 },
-      {
-        y: -80,
-        duration: 1.2,
-        ease: "power3.inOut",
-        stagger: 0.1,
-        delay: delay,
-      },
-      0
-    )
-    .to(
-      Text2.chars,
-      {
-        y: -80,
-        duration: 1.2,
-        ease: "power3.inOut",
-        stagger: 0.1,
-        delay: delay2,
-      },
-      1
-    );
 }
