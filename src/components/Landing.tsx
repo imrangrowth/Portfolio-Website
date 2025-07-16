cc5efbb08c86b0f36782b46cfab7cff63e64977d
Imranmuhaa <imranmuha43@gmail.com>
2025-07-17 00:10:52 +0100

    Update Landing.tsx

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,20 +9,21 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              MONCY
+              IMRAN
               <br />
-              <span>YOHANNAN</span>
+              <span>MUHAMMAD</span>
             </h1>
           </div>
           <div className="landing-info">
-            <h3>A Creative</h3>
+            <h3>A Passionate</h3>
             <h2 className="landing-info-h2">
-              <div className="landing-h2-1">Designer</div>
-              <div className="landing-h2-2">Developer</div>
+              <div className="landing-h2-1">Web Developer</div>
+              <div className="landing-h2-2">Designer</div>
+              <div className="landing-h2-1">Digital Marketer</div>
             </h2>
             <h2>
-              <div className="landing-h2-info">Developer</div>
-              <div className="landing-h2-info-1">Designer</div>
+              <div className="landing-h2-info">Creative Strategist</div>
+              <div className="landing-h2-info-1">Growth Expert</div>
             </h2>
           </div>
         </div>
