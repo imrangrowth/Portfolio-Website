85c4cd20c05f0b77294bbc5116537b1cf5c8651e
Imranmuhaa <imranmuha43@gmail.com>
2025-07-17 01:38:18 +0100

    Update WorkImage.tsx

--- src/components/WorkImage.tsx
+++ src/components/WorkImage.tsx
@@ -4,20 +4,18 @@ import { MdArrowOutward } from "react-icons/md";
 interface Props {
   image: string;
   alt?: string;
-  video?: string;
+  video?: string; // Name of video file in /public/videos/
   link?: string;
 }
 
-const WorkImage = (props: Props) => {
+const WorkImage = ({ image, alt, video, link }: Props) => {
   const [isVideo, setIsVideo] = useState(false);
-  const [video, setVideo] = useState("");
-  const handleMouseEnter = async () => {
-    if (props.video) {
+  const [videoUrl, setVideoUrl] = useState("");
+
+  const handleMouseEnter = () => {
+    if (video) {
       setIsVideo(true);
-      const response = await fetch(`src/assets/${props.video}`);
-      const blob = await response.blob();
-      const blobUrl = URL.createObjectURL(blob);
-      setVideo(blobUrl);
+      setVideoUrl(`/videos/${video}`); // Ensure video is placed in public/videos
     }
   };
 
@@ -25,19 +23,22 @@ const WorkImage = (props: Props) => {
     <div className="work-image">
       <a
         className="work-image-in"
-        href={props.link}
+        href={link}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={() => setIsVideo(false)}
         target="_blank"
-        data-cursor={"disable"}
+        rel="noopener noreferrer"
+        data-cursor="disable"
       >
-        {props.link && (
+        {link && (
           <div className="work-link">
             <MdArrowOutward />
           </div>
         )}
-        <img src={props.image} alt={props.alt} />
-        {isVideo && <video src={video} autoPlay muted playsInline loop></video>}
+        <img src={image} alt={alt} />
+        {isVideo && video && (
+          <video src={videoUrl} autoPlay muted playsInline loop></video>
+        )}
       </a>
     </div>
   );
