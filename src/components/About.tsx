7cd7dd53cfe4b8dc27e73c9496456f81cd9784d2
Imranmuhaa <imranmuha43@gmail.com>
2025-12-31 14:52:08 +0100

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-          I'm Imran Muhammad — a passionate digital marketer, web designer, and full-stack developer. 
+          I'm Imran Muhaa — a passionate digital marketer, web designer, and full-stack developer. 
           I help individuals and businesses grow their brands online through impactful design, 
           functional websites, and smart marketing strategies. Whether you're launching a startup or 
           expanding your digital presence, I'm here to help you succeed with clean, professional, and 
